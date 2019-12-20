d0bc74209535985c1ab13d5c8b4356b74f3c8759
Geohound90 <geohound90@gmail.com>
2019-12-20 17:38:20 +0300

    Revert "Отладка программы в IDEA[#211789]"

---
+++ chapter_001/src/test/java/ru/job4j/condition/SqMaxTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SqMaxTest {
+
+    @Test
+    public void whenFirstMax() {
+        SqMax check = new SqMax();
+        int result = check.max(5, 4, 2, 3);
+        assertThat(result, is(5));
+    }
+
+    @Test
+    public void whenSecondMax() {
+        SqMax check = new SqMax();
+        int result = check.max(5,7,4,2);
+        assertThat(result, is(7));
+    }
+
+    @Test
+    public void whenThirdMax() {
+        SqMax check = new SqMax();
+        int result = check.max(3,4,9,7);
+        assertThat(result, is(9));
+    }
+
+    @Test
+    public void whenForthMax() {
+        SqMax check = new SqMax();
+        int result = check.max(3,7,2,8);
+        assertThat(result, is(8));
+    }
+}
