d79490a4519316caf2670f5a2077962c10b22921
Geohound90 <geohound90@gmail.com>
2019-12-21 22:19:35 +0300

    Revert "Создать программу, вычисляющую факториал.[#211858]"

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -4,7 +4,6 @@ public class Factorial {
     public int calc(int n) {
         int result = 1;
         for (int i = 1; i<= n; i++) {
-            if (result == 0) result = i;
             result = result * i;
         }
         return result;
