fca85ced57b5e3606737fbd80d97df03ca294d2b
Geohound90 <geohound90@gmail.com>
2019-12-22 15:53:49 +0300

    JavaDoc. Code convention[#211860]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,26 +1,67 @@
 package ru.job4j.calculator;
 
+/**
+ * Calculator
+ *
+ * @author Maksim Pavlov (geohoun90@gmail.com)
+ */
 public class Calculator {
+    /**
+     * Method add.
+     * @param first First number.
+     * @param second Second number.
+     */
     public static void add(double first, double second) {
+        /**
+         * Contains the result of adding the first and second
+         */
         double result = first + second;
         System.out.println(first + " + " + second + " = " + result);
     }
 
+    /**
+     * Method div.
+     * @param first First number.
+     * @param second Second number.
+     */
     public static void div(double first, double second) {
+        /**
+         * Contains the result of dividing the first and second
+         */
         double result = first / second;
         System.out.println(first + " / " + second + " = " + result);
     }
 
+    /**
+     * Method multiply.
+     * @param first First number.
+     * @param second Second number.
+     */
     public static void multiply(double first, double second) {
+        /**
+         * Contains the result of multiplying the first and second
+         */
         double result = first * second;
         System.out.println(first + " * " + second + " = " + result);
     }
 
+    /**
+     * Method subtrack.
+     * @param first First number.
+     * @param second Second number.
+     */
     public static void subtrack(double first, double second) {
+        /**
+         * Contains the result of subtracting the first and second
+         */
         double result = first - second;
         System.out.println(first + " - " + second + " = " + result);
     }
 
+    /**
+     * Main
+     * @param args - args.
+     */
     public static void main(String[] args) {
         add(1, 1);
         div(4,2);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculator task.
+ *
+ * @author Pavlov Maksim (geohound90@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j.calculator;
