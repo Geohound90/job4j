2da8bb3d8eeae7f68f64709abb670220275a9d4b
Geohound90 <geohound90@gmail.com>
2019-12-24 22:10:54 +0300

    Слово заканчивается с ...[#211802]

--- chapter_001/src/main/java/ru/job4j/array/EndsWith.java
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -1,10 +1,12 @@
 package ru.job4j.array;
 
 public class EndsWith {
+
     public static boolean endsWithBack(char[] word, char[] post) {
         boolean result = false;
-        for (int index = word.length - 1; index != (word.length - post.length - 1); index--) {
-            if (word[index] == post[index - (word.length - post.length)]) {
+        int size = word.length - post.length;
+        for (int index = word.length - 1; index != (size - 1); index--) {
+            if (word[index] == post[index - size]) {
                 result = true;
             } else {
                 result = false;
@@ -16,8 +18,9 @@ public class EndsWith {
 
     public static boolean endsWithForward(char[] word, char[] post) {
         boolean result = false;
-        for (int index = (word.length - post.length + 1); index != word.length; index++) {
-            if (word[index] == post[index - (word.length - post.length)]) {
+        int size = word.length - post.length;
+        for (int index = (size + 1); index != word.length; index++) {
+            if (word[index] == post[index - size]) {
                 result = true;
             } else {
                 result = false;
