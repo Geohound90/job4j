2c67ce835df1ad73c16b4097bdf25d4dad325ec1
Geohound90 <geohound90@gmail.com>
2019-12-27 13:50:04 +0300

    Выигрышные комбинации в сокобан[#211800]

---
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,53 @@
+package ru.job4j.array;
+
+public class MatrixCheck {
+
+    public static boolean monoHorizontal(char[][] board, int row) {
+        boolean result = false;
+        for (int cell = 0; cell != board.length; cell++) {
+            if (board[row][cell] == 'X') {
+                result = true;
+            } else {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean monoVertical(char[][] board, int cell) {
+        boolean result = false;
+        for (int row = 0; row != board.length; row++) {
+            if (board[row][cell] == 'X') {
+                result = true;
+            } else {
+                result = false;
+                break;
+            };
+        }
+        return result;
+    }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int row = 0; row != board.length; row++) {
+            for (int cell = 0; cell != board.length; cell++) {
+                if (board[row][cell] == 'X') {
+                    if (monoHorizontal(board, row) == true) {
+                        result = true;
+                        break;
+                    } else if (monoVertical(board, cell) == true) {
+                        result = true;
+                        break;
+                    } else {
+                        result = false;
+                    }
+                }
+            }
+            if (result == true) {
+                break;
+            }
+        }
+        return result;
+    }
+}
