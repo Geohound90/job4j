96de9cf2d0541664baeffef779fb46b3c88e4358
Geohound90 <geohound90@gmail.com>
2020-01-15 19:34:26 +0300

    4. Переопределение[#211948]

---
+++ chapter_001/src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,10 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport {
+    public String generate(String name, String body) {
+        return "{" + '\n' +
+                '\n' + " " + name.substring(9) + " : " + name.substring(9) + "," +
+                '\n' + " " + body.substring(9) + " : " + body.substring(9) + '\n' +
+                '\n' + "}";
+    }
+}
