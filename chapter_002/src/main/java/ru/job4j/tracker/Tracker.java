283a4f43cf2a9dbd04dcef7dc9efa38834e5342f
Geohound90 <geohound90@gmail.com>
2020-01-17 19:36:52 +0300

    5. Реализовать класс Tracker[#211957]

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,4 +1,58 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Tracker {
+    private final Item[] items = new Item[100];
+
+    private int position = 0;
+
+    public Item add(Item item) {
+        item.setId(this.generateId());
+        items[this.position++] = item;
+        return item;
+    }
+
+    private String generateId() {
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
+    }
+
+    public Item[] findAll() {
+        Item[] item = new Item[position];
+        int size = 0;
+        for (int index = 0; index < position; index++) {
+            if (this.items[index] != null) {
+                item[size] = this.items[index];
+                size++;
+            }
+        }
+        item = Arrays.copyOf(item, size);
+        return item;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] item = new Item[position];
+        int size = 0;
+        for (int index = 0; index < position; index++) {
+            if (this.items[index].getName().equals(key)) {
+                item[size] = this.items[index];
+                size++;
+            }
+        }
+        item = Arrays.copyOf(item, size);
+        return item;
+    }
+
+    public Item findById(String id) {
+        Item item = null;
+        for (int index = 0; index < position; index++) {
+            if (this.items[index].getId().equals(id)) {
+                item = this.items[index];
+                break;
+            }
+        }
+        return item;
+    }
 }
---
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class TrackerTest {
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getName(), is(item.getName()));
+    }
+
+    @Test
+    public void showAllItems() {
+        Tracker tracker = new Tracker();
+        Item[] item = new Item[2];
+        for (int i = 0; i < item.length; i++) {
+            item[i] = new Item("name" + (i + 1));
+            tracker.add(item[i]);
+        }
+        Item[] result = tracker.findAll();
+        assertThat(result, is(item));
+}
+
+    @Test
+    public void findItemById() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("name1");
+        tracker.add(item);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getId(), is(item.getId()));
+    }
+
+    @Test
+    public void findItemByName() {
+        Tracker tracker = new Tracker();
+        Item[] item = new Item[4];
+        for (int i = 0; i < item.length; i++) {
+            item[i] = new Item("name" + (i + 1));
+            tracker.add(item[i]);
+        }
+        item[3].setName("name1");
+        Item[] control = new Item[] {item[0], item[3]};
+        Item[] result = tracker.findByName("name1");
+        assertThat(result, is(control));
+    }
+}
