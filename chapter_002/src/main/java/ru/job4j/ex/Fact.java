966cc8be3ca723c5ff75afe3dd5b88fe6a788037
Geohound90 <geohound90@gmail.com>
2020-02-04 19:54:23 +0300

    0.2. Кидаем исключение - throw new RuntimeException

---
+++ chapter_002/src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,19 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static int calc(int n) {
+        if (n < 1) {
+            throw new IllegalArgumentException("Parameter 'n' must be equal to or higher than 1.");
+        }
+        int rsl = 1;
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        int num = 0;
+        System.out.println("Factorial of " + num + " is: " + calc(num));
+    }
+}
