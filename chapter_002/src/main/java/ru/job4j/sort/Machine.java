d1f0fa359d58a33c55718bd9bd86dfc90bd83d42
Geohound90 <geohound90@gmail.com>
2020-02-28 17:47:22 +0300

    Сдача в кофе машине.

---
+++ chapter_002/src/main/java/ru/job4j/sort/Machine.java
@@ -0,0 +1,21 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+public class Machine {
+    private final int[] COINS = {10, 5, 2, 1};
+
+    public int[] change(int money, int price) {
+        int[] rsl = new int[100];
+        int size = 0;
+        int balance = money - price;
+        for (int index = 0; index < COINS.length; index++) {
+            while ((balance > 0) && (balance >= COINS[index])) {
+                balance -= COINS[index];
+                rsl[size] = COINS[index];
+                size++;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+}
