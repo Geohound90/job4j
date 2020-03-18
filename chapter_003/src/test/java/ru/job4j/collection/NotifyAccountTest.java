3eac36502791a6d9740db1d61b10d899adc82e92
Geohound90 <geohound90@gmail.com>
2020-03-18 20:24:43 +0300

    3. Модели данных и HashSet. [#218530]

---
+++ chapter_003/src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+
+public class NotifyAccountTest {
+    @Test
+    public void sent() {
+        List<Account> accounts = Arrays.asList(
+                new Account("123", "Maxim Pavlov", "aTerc7822g"),
+                new Account("321", "Maxim Pavlov", "0001")
+        );
+        HashSet<Account> expect = new HashSet<>(
+                Arrays.asList(
+                    new Account("123", "Maxim Pavlov", "aTerc7822g"),
+                    new Account("321", "Maxim Pavlov", "0001")
+                )
+        );
+        assertThat(NotifyAccount.sent(accounts), is(expect));
+    }
+
+    @Test
+    public void deleteDuplicatePassport() {
+        List<Account> accounts = Arrays.asList(
+                new Account("123", "Maxim Pavlov", "aTerc7822g"),
+                new Account("321", "Maxim Pavlov", "0001"),
+                new Account("321", "Maxim Pavlov", "3211")
+        );
+        HashSet<Account> expect = new HashSet<>(
+                Arrays.asList(
+                        new Account("123", "Maxim Pavlov", "aTerc7822g"),
+                        new Account("321", "Maxim Pavlov", "0001")
+                )
+        );
+        assertThat(NotifyAccount.sent(accounts), is(expect));
+    }
+
+}
