229792d2f0b1005044dc5977290555160811ce8b
Geohound90 <geohound90@gmail.com>
2020-04-07 15:35:41 +0300

    Отсортировать департаменты [#34352]

---
+++ chapter_003/src/main/java/ru/job4j/collection/DepDescComp.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepDescComp implements Comparator<String> {
+
+    @Override
+    public int compare(String first, String second) {
+        int rsl = 0;
+        String[] left = first.split("/");
+        String[] right = second.split("/");
+        int comp = right[0].compareTo(left[0]);
+        if (comp < 0) {
+            rsl = second.compareTo(first);
+        } else if (comp == 0) {
+            rsl = first.compareTo(second);
+        }
+        return rsl;
+    }
+}
---
+++ chapter_003/src/main/java/ru/job4j/collection/Departments.java
@@ -0,0 +1,49 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class Departments {
+
+    public static List<String> fillGaps(List<String> deps) {
+        HashSet<String> tmp = new HashSet<>();
+        for (String value : deps) {
+            int index = 0;
+            String[] splited = value.split("/");
+            String start = splited[0];
+            for (String el : splited) {
+                if (index == 0) {
+                    tmp.add(start);
+                } else {
+                    start += "/" + el;
+                    tmp.add(start);
+                }
+                index++;
+            }
+        }
+        return new ArrayList<>(tmp);
+    }
+
+    public static void sortAsc(List<String> orgs) {
+        orgs.sort(Comparator.naturalOrder());
+    }
+
+    public static void sortDesc(List<String> orgs) {
+        Collections.sort(orgs, new DepDescComp());
+    }
+
+    public static void main(String[] args) {
+        List<String> input = Arrays.asList("K1/SK1",
+                "K1/SK2",
+                "K1/SK1/SSK1",
+                "K1/SK1/SSK2",
+                "K2",
+                "K2/SK1/SSK1",
+                "K2/SK1/SSK2");
+        List<String> deps = Departments.fillGaps(input);
+        System.out.println(deps);
+        Departments.sortAsc(deps);
+        System.out.println(deps);
+        Departments.sortDesc(deps);
+        System.out.println(deps);
+    }
+}
