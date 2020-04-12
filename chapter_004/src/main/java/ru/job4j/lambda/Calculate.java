2afbb916c8c354a39b561fc192cb0a6015fef7a1
Geohound90 <geohound90@gmail.com>
2020-04-12 11:51:43 +0300

    1. Подсчет функции в диапазоне.[#211755]

--- chapter_004/src/main/java/ru/job4j/lambda/Calculate.java
+++ chapter_004/src/main/java/ru/job4j/lambda/Calculate.java
@@ -5,32 +5,11 @@ import java.util.List;
 import java.util.function.Function;
 
 public class Calculate {
-    private static List<Double> diapason(int start, int end, Function<Double, Double> func) {
+    public List<Double> diapason(int start, int end, Function<Double, Double> func) {
         List<Double> rsl= new ArrayList<>();
         for (int index = start; index < end; index++) {
             rsl.add(func.apply((double) index));
         }
         return rsl;
-    }
-
-    public List<Double> linear(int start, int end) {
-        Function<Double, Double> func = (x) -> {
-            return (2 * x) + 1;
-        };
-        return diapason(start, end, func);
-    }
-
-    public List<Double> quadratic(int start, int end) {
-        Function<Double, Double> func = (x) -> {
-            return Math.pow(x, 2D);
-        };
-        return diapason(start, end, func);
-    }
-
-    public List<Double> exponential(int start, int end) {
-        Function<Double, Double> func = (x) -> {
-            return Math.pow(2D, x);
-        };
-        return diapason(start, end, func);
     }
 }
--- chapter_004/src/test/java/ru/job4j/lambda/FuncTest.java
+++ chapter_004/src/test/java/ru/job4j/lambda/FuncTest.java
@@ -3,6 +3,7 @@ package ru.job4j.lambda;
 import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -11,7 +12,7 @@ public class FuncTest {
     @Test
     public void whenLinearFunctionThenLinearResults() {
         Calculate calc = new Calculate();
-        List<Double> result = calc.linear(5, 8);
+        List<Double> result = calc.diapason(5, 8, (x) -> 2 * x + 1);
         List<Double> expected = Arrays.asList(11D, 13D, 15D);
         assertThat(result, is(expected));
     }
@@ -19,14 +20,14 @@ public class FuncTest {
     @Test
     public void whenQuadraticFunctionThenQuadraticResults() {
         Calculate calc = new Calculate();
-        List<Double> result = calc.quadratic(5, 8);
+        List<Double> result = calc.diapason(5, 8, (x) -> Math.pow(x, 2D));
         List<Double> expected = Arrays.asList(25D, 36D, 49D);
         assertThat(result, is(expected));
     }
     @Test
     public void whenExponentialFunctionThenExponentialResults() {
         Calculate calc = new Calculate();
-        List<Double> result = calc.exponential(2, 5);
+        List<Double> result = calc.diapason(2, 5, (x) -> Math.pow(2D, x));
         List<Double> expected = Arrays.asList(4D, 8D, 16D);
         assertThat(result, is(expected));
     }
