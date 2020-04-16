0d7b56f028cdea439109f5106d8e21c2d25bbfeb
Geohound90 <geohound90@gmail.com>
2020-04-16 12:29:40 +0300

    2. Список адресов[#211764]

--- chapter_004/src/main/java/ru/job4j/stream/Profiles.java
+++ chapter_004/src/main/java/ru/job4j/stream/Profiles.java
@@ -1,36 +1,12 @@
 package ru.job4j.stream;
 
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
-        List<Address> rsl = profiles.stream().map(
+        return profiles.stream().map(
                 profile -> profile.getAddress()
         ).collect(Collectors.toList());
-        Comparator<Address> comparator = (left, right) -> left.getCity().compareTo(right.getCity());
-        rsl.sort(comparator);
-        return rsl;
-    }
-
-    public List<Address> filter(List<Address> profiles) {
-        List<Address> rsl = profiles.stream().distinct().collect(Collectors.toList());
-        return rsl;
-    }
-
-    public static void main(String[] args) {
-        Profiles profiles = new Profiles();
-        List<Profile> profile = List.of(
-                new Profile(new Address("Moscow", "Uchinskaya", 10, 30)),
-                new Profile(new Address("Bryansk", "Lobnenskaya", 8, 100)),
-                new Profile(new Address("Podolsk", "Ikshinskaya", 16, 45)),
-                new Profile(new Address("Astrahan", "Dolgoprudnaya", 1, 26)),
-                new Profile(new Address("Podolsk", "Ikshinskaya", 16, 45)),
-                new Profile(new Address("Podolsk", "Angarskaya", 7, 4))
-        );
-        List<Address> sorted = profiles.collect(profile);
-        profiles.filter(sorted).forEach(System.out::println);
     }
 }
---
+++ chapter_004/src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ProfilesTest {
+    @Test
+    public void whenCollect() {
+        Profiles profiles = new Profiles();
+        List<Profile> profile = List.of(
+                new Profile(new Address("Moscow", "Uchinskaya", 10, 30)),
+                new Profile(new Address("Bryansk", "Lobnenskaya", 8, 100)),
+                new Profile(new Address("Podolsk", "Ikshinskaya", 16, 45)),
+                new Profile(new Address("Astrahan", "Dolgoprudnaya", 1, 26)),
+                new Profile(new Address("Podolsk", "Ikshinskaya", 16, 45)),
+                new Profile(new Address("Podolsk", "Angarskaya", 7, 4))
+        );
+        List<Address> result = profiles.collect(profile);
+        List<Address> expected = List.of(
+                new Address("Moscow", "Uchinskaya", 10, 30),
+                new Address("Bryansk", "Lobnenskaya", 8, 100),
+                new Address("Podolsk", "Ikshinskaya", 16, 45),
+                new Address("Astrahan", "Dolgoprudnaya", 1, 26),
+                new Address("Podolsk", "Ikshinskaya", 16, 45),
+                new Address("Podolsk", "Angarskaya", 7, 4)
+        );
+        assertThat(result, is(expected));
+    }
+}
