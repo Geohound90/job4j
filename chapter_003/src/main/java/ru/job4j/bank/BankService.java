9711eac1d6eae646409073232d6b2435b4b65162
Geohound90 <geohound90@gmail.com>
2020-04-17 16:00:37 +0300

    6. Тестовое задание из модуля коллекции Lite переделать на Stream API.[#211761]

--- chapter_003/src/main/java/ru/job4j/bank/BankService.java
+++ chapter_003/src/main/java/ru/job4j/bank/BankService.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class BankService {
     private Map<User, List<Account>> users = new HashMap<>();
@@ -13,33 +15,28 @@ public class BankService {
     }
 
     public void addAccount(String passport, Account account) {
-        User user = findByPassport(passport);
-        if (user != null) {
-            if (findByRequisite(passport, account.getRequisite()) == null) {
-                users.get(user).add(account);
+        List<User> user = findByPassport(passport);
+        if (user.size() != 0) {
+            if (findByRequisite(passport, account.getRequisite()).size() == 0) {
+                users.get(user.get(0)).add(account);
             }
         }
     }
 
-    public User findByPassport(String passport) {
-        User user = null;
-        for (User key : users.keySet()) {
-            if (key.getPassport().equals(passport)) {
-                user = key;
-            }
-        }
+    public List<User> findByPassport(String passport) {
+        List<User> user = users.keySet().stream()
+                .filter(pass -> pass.getPassport().equals(passport))
+                .collect(Collectors.toList());
         return user;
     }
 
-    public Account findByRequisite(String passport, String requisite) {
-        User user = findByPassport(passport);
-        Account account = null;
-        if (user != null) {
-            for (Account acc : users.get(user)) {
-                if (acc.getRequisite().equals(requisite)) {
-                    account = acc;
-                }
-            }
+    public List<Account> findByRequisite(String passport, String requisite) {
+        List<User> user = findByPassport(passport);
+        List<Account> account = null;
+        if (user.size() != 0) {
+            account = users.get(user.get(0)).stream()
+                    .filter(req -> req.getRequisite().equals(requisite))
+                    .collect(Collectors.toList());
         }
         return account;
     }
@@ -47,11 +44,11 @@ public class BankService {
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
-        Account srcAccount = findByRequisite(srcPassport, srcRequisite);
-        Account destAccount = findByRequisite(destPassport, destRequisite);
-        if (srcAccount != null && destAccount != null && (srcAccount.getBalance() >= amount)) {
-            destAccount.setBalance(destAccount.getBalance() + amount);
-            srcAccount.setBalance(srcAccount.getBalance() - amount);
+        List<Account> srcAccount = findByRequisite(srcPassport, srcRequisite);
+        List<Account> destAccount = findByRequisite(destPassport, destRequisite);
+        if (srcAccount.size() != 0 && destAccount.size() != 0 && (srcAccount.get(0).getBalance() >= amount)) {
+            destAccount.get(0).setBalance(destAccount.get(0).getBalance() + amount);
+            srcAccount.get(0).setBalance(srcAccount.get(0).getBalance() - amount);
             rsl = true;
         }
         return rsl;
