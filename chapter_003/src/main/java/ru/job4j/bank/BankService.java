95a706d3706d76ed6cb2dc1565726fde9d353e20
Geohound90 <geohound90@gmail.com>
2020-04-23 13:12:20 +0300

    4. Optional в Stream API. [#279130]

--- chapter_003/src/main/java/ru/job4j/bank/BankService.java
+++ chapter_003/src/main/java/ru/job4j/bank/BankService.java
@@ -1,42 +1,37 @@
 package ru.job4j.bank;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.*;
 
 public class BankService {
     private Map<User, List<Account>> users = new HashMap<>();
 
-    public void addUser(User user) {
-        users.putIfAbsent(user, new ArrayList<Account>());
+    public void addUser(Optional<User> user) {
+        users.putIfAbsent(user.get(), new ArrayList<Account>());
     }
 
     public void addAccount(String passport, Account account) {
-        User user = findByPassport(passport);
-        if (user != null) {
-            if (!users.get(user).contains(account)) {
-                users.get(user).add(account);
+        Optional<User> user = findByPassport(passport);
+        if (user.isPresent()) {
+            if (!users.get(user.get()).contains(account)) {
+                users.get(user.get()).add(account);
             }
         }
     }
 
-    public User findByPassport(String passport) {
-        User user = users.keySet().stream()
+    public Optional<User> findByPassport(String passport) {
+        Optional<User> user = users.keySet().stream()
                 .filter(pass -> pass.getPassport().equals(passport))
-                .findFirst().orElse(null);
+                .findFirst();
         return user;
     }
 
-    public Account findByRequisite(String passport, String requisite) {
-        User user = findByPassport(passport);
-        Account account = null;
-        if (user != null) {
-            account = users.get(user).stream()
+    public Optional<Account> findByRequisite(String passport, String requisite) {
+        Optional<User> user = findByPassport(passport);
+        Optional<Account> account = null;
+        if (user.isPresent()) {
+            account = users.get(user.get()).stream()
                     .filter(req -> req.getRequisite().equals(requisite))
-                    .findFirst().orElse(null);
+                    .findFirst();
         }
         return account;
     }
@@ -44,14 +39,13 @@ public class BankService {
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
-        Account srcAccount = findByRequisite(srcPassport, srcRequisite);
-        Account destAccount = findByRequisite(destPassport, destRequisite);
-        if (srcAccount != null && destAccount != null && (srcAccount.getBalance() >= amount)) {
-            destAccount.setBalance(destAccount.getBalance() + amount);
-            srcAccount.setBalance(srcAccount.getBalance() - amount);
+        Optional<Account> srcAccount = findByRequisite(srcPassport, srcRequisite);
+        Optional<Account> destAccount = findByRequisite(destPassport, destRequisite);
+        if (srcAccount != null && destAccount != null && (srcAccount.get().getBalance() >= amount)) {
+            destAccount.get().setBalance(destAccount.get().getBalance() + amount);
+            srcAccount.get().setBalance(srcAccount.get().getBalance() - amount);
             rsl = true;
         }
         return rsl;
     }
-
 }
--- chapter_003/src/test/java/ru/job4j/bank/BankServiceTest.java
+++ chapter_003/src/test/java/ru/job4j/bank/BankServiceTest.java
@@ -2,13 +2,15 @@ package ru.job4j.bank;
 
 import org.junit.Test;
 
+import java.util.Optional;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class BankServiceTest {
     @Test
     public void addUser() {
-        User user = new User("12345", "Maxim Pavlov");
+        Optional<User> user = Optional.of(new User("12345", "Maxim Pavlov"));
         BankService bank = new BankService();
         bank.addUser(user);
         assertThat(bank.findByPassport("12345"), is(user));
@@ -16,30 +18,30 @@ public class BankServiceTest {
 
     @Test
     public void whenEnterInvalidPassport() {
-        User user = new User("12345", "Maxim Pavlov");
+        Optional<User> user = Optional.of(new User("12345", "Maxim Pavlov"));
         BankService bank = new BankService();
         bank.addUser(user);
-        bank.addAccount(user.getPassport(), new Account("5546", 150D));
+        bank.addAccount(user.get().getPassport(), new Account("5546", 150D));
         assertNull(bank.findByRequisite("34", "5546"));
     }
 
     @Test
     public void addAccount() {
-        User user = new User("12345", "Maxim Pavlov");
+        Optional<User> user = Optional.of(new User("12345", "Maxim Pavlov"));
         BankService bank = new BankService();
         bank.addUser(user);
-        bank.addAccount(user.getPassport(), new Account("6578", 150D));
-        assertThat(bank.findByRequisite("12345", "6578").getBalance(), is(150D));
+        bank.addAccount(user.get().getPassport(), new Account("6578", 150D));
+        assertThat(bank.findByRequisite("12345", "6578").get().getBalance(), is(150D));
     }
 
     @Test
     public void transferMoney() {
-        User user = new User("12345", "Maxim Pavlov");
+        Optional<User> user = Optional.of(new User("12345", "Maxim Pavlov"));
         BankService bank = new BankService();
         bank.addUser(user);
-        bank.addAccount(user.getPassport(), new Account("6578", 150D));
-        bank.addAccount(user.getPassport(), new Account("111", 50D));
-        bank.transferMoney(user.getPassport(), "6578", user.getPassport(), "111", 150D);
-        assertThat(bank.findByRequisite(user.getPassport(), "111").getBalance(), is(200D));
+        bank.addAccount(user.get().getPassport(), new Account("6578", 150D));
+        bank.addAccount(user.get().getPassport(), new Account("111", 50D));
+        bank.transferMoney(user.get().getPassport(), "6578", user.get().getPassport(), "111", 150D);
+        assertThat(bank.findByRequisite(user.get().getPassport(), "111").get().getBalance(), is(200D));
     }
 }
